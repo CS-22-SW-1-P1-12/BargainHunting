45c91cb4599d66f6ebe7fe01bb37ac56e0e91f16
mantarias <32459446+mantarias@users.noreply.github.com>
2022-11-03 13:49:05 +0100

    cmake fix

---
+++ cmake-build-debug/CMakeFiles/BargainHunting.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "BargainHunting.exe"
+  "BargainHunting.exe.manifest"
+  "BargainHunting.pdb"
+  "CMakeFiles/BargainHunting.dir/src/main.c.obj"
+  "CMakeFiles/BargainHunting.dir/src/main.c.obj.d"
+  "libBargainHunting.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/BargainHunting.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/BargainHunting.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for BargainHunting.
+# This may be replaced when dependencies are built.
