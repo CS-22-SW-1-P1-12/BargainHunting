0f5b5b98b04683e44013b381a83a8975f57498cd
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2022-11-03 13:45:27 +0100

    fuckit

---
+++ src/main.c
@@ -0,0 +1,5 @@
+//
+// Created by Lennart Diego Kahn on 03/11/2022.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Lennart Diego Kahn on 03/11/2022.
+//
+
+#ifndef BARGAINHUNTING_MAIN_H
+#define BARGAINHUNTING_MAIN_H
+
+#endif //BARGAINHUNTING_MAIN_H
