aa4c9990a602b49dcb7ef68dfe1bf2b315d3f3e3
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2022-11-17 12:20:14 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,38 +1,8 @@
 //
 // Created by Lennart Diego Kahn on 03/11/2022.
 //
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct Tag{
-    char name[24];
-    struct Tag* next_tag;
-};
-
-struct Product{
-    char name[24];
-    double price;
-    double ppk;
-    struct Tag* first_tag;
-};
 
 #include "main.h"
 int main(){
-
-
-    struct Product milk = {
-            "Milk",
-            12.0,
-            12.0,
-            NULL,
-    };
-
-    struct Tag* new_tag = (struct Tag*) malloc(sizeof(struct Tag));
-    new_tag->next_tag = milk.first_tag;
-    strcpy(new_tag->name, "REMA 1000");
-    milk.first_tag = new_tag,
-
-
-    printf("NAME: %s\nPRICE: %lf\nPRICE PER KG: %lf\nSTORE: %s", milk.name, milk.price, milk.ppk, milk.first_tag->name);
+    
 }
