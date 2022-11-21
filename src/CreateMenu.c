2ede3c96cc62f1dae0db76d5d90677163f40c6cf
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2022-11-21 11:57:43 +0100

    init

---
+++ src/CreateMenu.c
@@ -0,0 +1,5 @@
+//
+// Created by Lennart Diego Kahn on 21/11/2022.
+//
+
+#include "CreateMenu.h"
---
+++ src/CreateMenu.h
@@ -0,0 +1,33 @@
+//
+// Created by Lennart Diego Kahn on 21/11/2022.
+//
+
+#ifndef BARGAINHUNTING_CREATEMENU_H
+#define BARGAINHUNTING_CREATEMENU_H
+
+#endif //BARGAINHUNTING_CREATEMENU_H
+
+int Counter(char* options){
+    // return the amount of elements inside of options
+    return 7;
+}
+
+
+void Draw(char* options){
+
+}
+
+int ScanInput(){
+    
+}
+
+int CreateMenu(char* options){
+
+    return
+}
+
+
+
+void tests(){
+
+}
