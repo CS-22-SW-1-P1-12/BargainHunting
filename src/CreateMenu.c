f083a603e32c07a357b73867c2e1fe524b5a0925
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2022-11-21 12:02:52 +0100

    moved into .c and made .h

--- src/CreateMenu.c
+++ src/CreateMenu.c
@@ -3,3 +3,31 @@
 //
 
 #include "CreateMenu.h"
+
+// return the amount of elements inside of options
+int Counter(char* options){
+    return 7;
+}
+
+// Draw the entire ui in a pretty way
+void Draw(char* options){
+
+}
+
+// return the selected option (1,2,3,4,....)
+int ScanInput(){
+    return 1;
+}
+
+
+// the main function for this block.
+int CreateMenu(char* options){
+
+    return ScanInput();
+}
+
+
+
+void tests(){
+
+}
