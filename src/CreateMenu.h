35a5ce1cfd12f0198cd0c1721ad965543a26a1b3
DeeKahy <97156953+DeeKahy@users.noreply.github.com>
2022-11-21 11:59:40 +0100

    init2

--- src/CreateMenu.h
+++ src/CreateMenu.h
@@ -7,23 +7,26 @@
 
 #endif //BARGAINHUNTING_CREATEMENU_H
 
+// return the amount of elements inside of options
 int Counter(char* options){
-    // return the amount of elements inside of options
     return 7;
 }
 
-
+// Draw the entire ui in a pretty way
 void Draw(char* options){
 
 }
 
+// return the selected option (1,2,3,4,....)
 int ScanInput(){
-    
+    return 1;
 }
 
+
+// the main function for this block.
 int CreateMenu(char* options){
 
-    return
+    return ScanInput();
 }
 
 
