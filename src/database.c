cde5818a6b0784b9a1c09891e5a800a947a07b6b
mantarias <32459446+mantarias@users.noreply.github.com>
2022-11-26 02:50:23 +0100

    testing a different DB

--- src/database.c
+++ src/database.c
@@ -7,37 +7,74 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "database.h"
 
 int main(){
+    clock_t start, end;
+    /* Store start time here */
+    start = clock();
     data_t* data = LoadDatabase();
-    product_t*  product = data->firstProduct;
-    while(product != NULL)
-    {
-        printf("name: %s price: %.2lf kr. ppk: %.2lf weight: %.2lf kg store: %s tags: ",product->name,product->price,product->ppk,product->weight,product->store);
-        tag_t* tempTag = product->first_tag;
-        while(tempTag != NULL)
-        {
-            printf("%s, ",tempTag->name);
-            tempTag = tempTag->nextTag;
+//    product_t*  product = data->firstProduct;
+//    while(product != NULL)
+//    {
+//        printf("name: %s price: %.2lf kr. ppk: %.2lf weight: %.2lf kg store: %s tags: ",product->name,product->price,product->ppk,product->weight,product->store);
+//        tag_t* tempTag = product->first_tag;
+//        while(tempTag != NULL)
+//        {
+//            printf("%s, ",tempTag->name);
+//            tempTag = tempTag->nextTag;
+//        }
+//        printf("\n");
+//        product = product->nextProduct;
+//    }
+    end = clock();
+    product_t tempProduct;
+    tag_t tempTag;
+    for (int i = 0; i < data->productSize; ++i) {
+        tempProduct = data->products[i];
+        tempProduct;
+        for (int j = 0; j < data->linkTableSize; ++j) {
+            if(i == data->linkTable[j].indexOfProduct)
+            {
+                tempTag = data->tags[data->linkTable[j].indexOfTag];
+                tempTag;
+            }
         }
         printf("\n");
-        product = product->nextProduct;
     }
+
+    double duration = ((double)end - start)/CLOCKS_PER_SEC;
+    printf("Time taken to execute in seconds : %f \n", duration);
+    return 0;
 }
 data_t* LoadDatabase(){
     FILE* filePtr;
-    filePtr = fopen("./data/data.txt","r");
+    filePtr = fopen("data/data.txt","r");
     if(filePtr == NULL)
     {
         exit(-1);
     }
     data_t* data = malloc(sizeof(data_t));
-    data->firstProduct = NULL;
     char buffer[100];
-    int test = 0;
+    int test;
+    data->productSize = 0;
+    data->tagSize = 0;
+    data->linkTableSize = 0;
+    do{
+        fscanf(filePtr,"%[^\n]",buffer);
+        test = getc(filePtr);
+        data->productSize++;
+    }
+    while(test != EOF);
+    data->productSize--;
+    rewind(filePtr);
+
+    data->products = malloc(sizeof (product_t) * data->productSize-1);
+
     fscanf(filePtr,"%[^\n]",buffer);
     test =fgetc(filePtr);
+    int i = 0;
     while (test != EOF){
         product_t* newProduct= malloc(sizeof(product_t));
         char name[20];
@@ -61,6 +98,7 @@ data_t* LoadDatabase(){
         fgetc(filePtr);
         newProduct->first_tag = NULL;
         int stopRun = 0;
+
         while(!stopRun)
         {
             fscanf(filePtr,"%[^,]",&tag[0]);
@@ -73,16 +111,85 @@ data_t* LoadDatabase(){
             else{
                 fseek(filePtr, -1, SEEK_CUR);
             }
-            tag_t* newTag = malloc(sizeof(tag_t));;
-            newTag->name = malloc(sizeof(char) * GetStrLength(tag));
-            strcpy(newTag->name,tag);
+//            tag_t* newTag = malloc(sizeof(tag_t));;
+//            newTag->name = malloc(sizeof(char) * GetStrLength(tag));
+//            strcpy(newTag->name,tag);
+//
+//            newTag->nextTag = newProduct->first_tag;
+//            newProduct->first_tag = newTag;
+            if(data->tagSize == 0)
+            {
+                data->tagSize++;
+                data->linkTableSize++;
+                tag_t* newTag = malloc(sizeof(tag_t));;
+                newTag->name = malloc(sizeof(char) * GetStrLength(tag));
+                strcpy(newTag->name,tag);
+                data->tags = malloc(sizeof(tag_t)*data->tagSize);
+                data->linkTable = malloc(sizeof(link_table_t)*data->linkTableSize);
+                link_table_t newLink;
+                newLink.indexOfProduct = i;
+                newLink.indexOfTag =  0;
+                data->tags[data->tagSize-1] = *newTag;
+                data->linkTable[data->linkTableSize-1] = newLink;
+            }
+            else
+            {
+                int found = 0;
+                for (int j = 0; j < data->tagSize; ++j) {
+                    if(strcmp(tag,data->tags[j].name) == 0)
+                    {
+                        found = j+1;
+                    }
+                }
+                if(!found)
+                {
+                    data->tagSize++;
+                    data->linkTableSize++;
+                    tag_t* newTag = malloc(sizeof(tag_t));;
+                    newTag->name = malloc(sizeof(char) * GetStrLength(tag));
+                    strcpy(newTag->name,tag);
+                    tag_t* tags = malloc(sizeof(tag_t)*data->tagSize);
+                    for(int j = 0; j <data->tagSize-1; j++ )
+                    {
+                        tags[j] = data->tags[j];
+                    }
+                    free(data->tags);
+                    data->tags = tags;
+                    link_table_t* linkTable = malloc(sizeof(link_table_t)*data->linkTableSize);
+                    for(int j = 0; j <data->linkTableSize-1; j++ )
+                    {
+                        linkTable[j] = data->linkTable[j];
+                    }
+                    free(data->linkTable);
+                    data->linkTable = linkTable;
+                    link_table_t newLink;
+                    newLink.indexOfProduct = i;
+                    newLink.indexOfTag =  data->tagSize-1;
+                    data->tags[data->tagSize-1] = *newTag;
+                    data->linkTable[data->linkTableSize-1] = newLink;
+                }
+                else
+                {
+                    data->linkTableSize++;
+                    link_table_t* linkTable = malloc(sizeof(link_table_t)*data->linkTableSize);
+                    for(int j = 0; j <data->linkTableSize-1; j++ )
+                    {
+                        linkTable[j] = data->linkTable[j];
+                    }
+                    free(data->linkTable);
+                    data->linkTable = linkTable;
+                    link_table_t newLink;
+                    newLink.indexOfProduct = i;
+                    newLink.indexOfTag =  found-1;
+                    data->linkTable[data->linkTableSize-1] = newLink;
+                }
 
-            newTag->nextTag = newProduct->first_tag;
-            newProduct->first_tag = newTag;
+            }
 
         }
-        newProduct->nextProduct = data->firstProduct;
-        data->firstProduct = newProduct;
+
+        data->products[i] = *newProduct;
+        i++;
         test =fgetc(filePtr);
     }
     fclose(filePtr);
--- src/database.h
+++ src/database.h
@@ -4,24 +4,31 @@
 
 #ifndef BARGAINHUNTING_DATABASE_H
 #define BARGAINHUNTING_DATABASE_H
-typedef struct tag_t {
+typedef struct {
     char* name;
-    struct tag_t* nextTag;
 }tag_t;
 //struct for products - first five vars are expected to always be included, with an optional number of tags also points to next product or null if it is the last one
-typedef struct product_t{
+typedef struct {
     char* name;
     double price;
     double ppk;
     double weight;
     char* store;
     tag_t* first_tag;
-    struct product_t* nextProduct;
 }product_t;
+typedef struct {
+    int indexOfTag;
+    int indexOfProduct;
+}link_table_t;
 
 //a combined struct to access all data includes a pointer to first product
 typedef struct {
-    product_t *firstProduct;
+    product_t * products;
+    int productSize;
+    tag_t* tags;
+    int tagSize;
+    link_table_t * linkTable;
+    int linkTableSize;
 }data_t;
 /***
  * function that load the data in and returns in
