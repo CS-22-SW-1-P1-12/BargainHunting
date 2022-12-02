7113fb114aee5fbfdc80015fba1aed10d7710841
sinagaming69 <sgrun22@student.aau.dk>
2022-12-02 11:20:57 +0100

    the code works now

--- main.c
+++ main.c
@@ -2,77 +2,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int sizejumps = 20;
-char **loadfile(char *filename, int *len);
-
-/*int main(int argc, char *argv[])
-{
-    if(argc == 1)
-    {
-        printf("not your day huh?");
-        exit(1);
-    }
-
-    int length = 0;
-   char **listFile = loadfile(argv[1],&length);
-
-    printf("%d\n",length);
+#define SIZE_JUMPS 1
+#define MAX_BUFFER 100
 
 
-}*/
+char **LoadFile(char *fileName);
+void PrintList(char** lines);
 
 void * main()
 {
-    FILE *file = fopen("listFile.txt", "r");
+    char* fileName = "listFile.txt";
+    char** lines = LoadFile(fileName);
+    PrintList(lines);
+    return 0;
+}
 
+//This function opens a text file and returns the contents in a string array called lines
+char **LoadFile(char *fileName){
+    FILE *file = fopen(fileName, "r");
+
+    //If the file is not found, print an error and return nothing
     if(!file)
     {
         printf("can't open");
         return NULL;
     }
 
-    int arraylenght = sizejumps;
+    int arrayLength = SIZE_JUMPS;
 
-    char **line = (char**) malloc(arraylenght * sizeof(char*));//allocate space for chars
+    char **lines = (char**) malloc(arrayLength * sizeof(char*));//allocate space for chars
 
-    char buffer[100];
+    char buffer[MAX_BUFFER];
     int i = 0;
 
-    while(fgets(buffer,100,file))
+    //This loop continues until there are no more strings in the file
+    while(fgets(buffer,MAX_BUFFER,file))
     {
-        if (i == arraylenght)
+        //If the current line is greater than the max number of lines, allocate space for another
+        if (i == arrayLength)
         {
-            arraylenght +=sizejumps;
-            char ** newline = realloc(line,arraylenght * sizeof(char*));
+            arrayLength += SIZE_JUMPS;
+            char ** newline = realloc(lines, arrayLength * sizeof(char*));
             if(!newline)
             {
                 printf("can't reallocate, sorry\n");
                 exit(1);
             }
-            line = newline;
+            lines = newline;
         }
 
-        int blen = strlen(buffer); // getting length of buffer
+        int bufferLength = strlen(buffer); // getting length of buffer
 
-        char *item = (char *) malloc((blen+1) * sizeof(char));// alocate space for string
+        char *item = (char *) malloc((bufferLength + 1) * sizeof(char)); // allocate space for string
 
-        strcpy(item,buffer);//copy string from buffer to item
-        line[i]=item;
+        strcpy(item,buffer); //copy string from buffer to item
+        lines[i] = item;
         i++;
 
     }
-    line[i] = NULL;
-    for (int j = 0; line[i] != NULL ;j++)
-    {
-        printf("%s\n",line[j]);
-    }
+    lines[i] = NULL;
+
     printf("done\n");
 
-    return line;
-
+    return lines;
 }
 
-
-
-
-
+//This function prints every string in a string array as a new line.
+void PrintList(char** lines){
+    for (int j = 0; lines[j] != NULL; j++)
+    {
+        printf("%s", lines[j]);
+    }
+    printf("\n");
+}
