cf6151143d73e7203dd4ae9ce37dddfc4283726b
mantarias <32459446+mantarias@users.noreply.github.com>
2022-12-05 10:06:07 +0100

    TBD

--- src/ListEditor.c
+++ src/ListEditor.c
@@ -7,49 +7,51 @@
 
 /*
  * todo:
- *  shoppingList[20][50] is shitty
+ *  shoppingList[MAX_OPTIONS][MAX_STRLEN] is shitty
  */
 
 void GetExistingLists(char *test) {
-    char arr[20][50] = {"Create New List"};
+	char arr[MAX_OPTIONS][MAX_STRLEN] = {"Create New List", "Go Back"};
 
-    int count2 = 1;
-    DIR *dr;
-    struct dirent *en;
-    dr = opendir("./data/shopping_list/"); //open all or present directory
-    if (dr) {
-        while ((en = readdir(dr)) != NULL) {
-            if (strcmp(en->d_name, ".") == 0 || strcmp(en->d_name, "..") == 0) {
-                continue;
-            }
-            strcpy(arr[count2], en->d_name);
-            count2++;
-        }
-        closedir(dr); //close all directory
-    }
+	int count2 = 2;
+	DIR *dr;
+	struct dirent *en;
+	dr = opendir("./data/shopping_list/"); //open all or present directory
+	if(dr) {
+		while((en = readdir(dr)) != NULL) {
+			if(strcmp(en->d_name, ".") == 0 || strcmp(en->d_name, "..") == 0) {
+				continue;
+			}
+			strcpy(arr[count2], en->d_name);
+			count2 ++;
+		}
+		closedir(dr); //close all directory
+	}
 
 
-    int result = CreateMenu(arr);
+	int result = CreateMenu(arr);
 
-    if (result == 1) {
-        char newlist[50];
-        printf("Write your list name: \n");
-        scanf("%s", newlist);
-        FILE *filePtr;
-        char path[50] = "./data/shopping_list/";
-        strcat(path, newlist);
-        filePtr = fopen(path, "w");
-        if (filePtr == NULL) {
-            exit(EXIT_FAILURE);
-        }
-        fprintf(filePtr, " ");
-        fclose(filePtr);
-        strcpy(test, newlist);
-    } else if (count2 >= result) {
-        strcpy(test, arr[result - 1]);
-    }
-
+	if(result == 1) {
+		char newlist[MAX_STRLEN];
+		printf("Write your list name: \n");
+		scanf("%s", newlist);
+		FILE *filePtr;
+		char path[MAX_STRLEN] = "./data/shopping_list/";
+		strcat(path, newlist);
+		filePtr = fopen(path, "w");
+		if(filePtr == NULL) {
+			exit(EXIT_FAILURE);
+		}
+		fprintf(filePtr, " ");
+		fclose(filePtr);
+		strcpy(test, newlist);
+	} else if(result == 2) {
+		//letting function end
+	} else if(count2 >= result) {
+		strcpy(test, arr[result - 1]);
+	}
 }
+
 
 /*
  * making a selection list to get the user to select the list they want to modify
@@ -57,63 +59,63 @@ void GetExistingLists(char *test) {
  * todo: then printing out everything inside of the list
  */
 void ListEditor() {
-    char listName[50];
-    GetExistingLists(listName);
-    //printf("%s", listName);
-    char path[50] = "./data/shopping_list/";
-    strcat(path, listName);
+	char listName[MAX_STRLEN];
+	GetExistingLists(listName);
+	//printf("%s", listName);
+	char path[MAX_STRLEN] = "./data/shopping_list/";
+	strcat(path, listName);
 
-    /*
-     * printing out the contents of the selected list
-     */
-    FILE *filePtr;
-    filePtr = fopen(path, "a+");
-    if (filePtr == NULL) {
-        exit(EXIT_FAILURE);
-    }
-    char shoppingList[20][50] = {"exit", "adding an item"};
+	/*
+	 * printing out the contents of the selected list
+	 */
+	FILE *filePtr;
+	filePtr = fopen(path, "a+");
+	if(filePtr == NULL) {
+		exit(EXIT_FAILURE);
+	}
+	char shoppingList[MAX_OPTIONS][MAX_STRLEN] = {"exit", "adding an item"};
 
-    int checkingNextChar;
-    //checking for amount of lines in file to make data array
-    int count = 2;
-    do {
-        fscanf(filePtr, "%[^\n]", shoppingList[count]);
-        checkingNextChar = getc(filePtr);
-        printf("%s\n", shoppingList[count]);
-        count++;
-    } while (checkingNextChar != EOF);
+	int checkingNextChar;
+	//checking for amount of lines in file to make data array
+	int count = 2;
+	do {
+		fscanf(filePtr, "%[^\n]", shoppingList[count]);
+		checkingNextChar = getc(filePtr);
+		printf("%s\n", shoppingList[count]);
+		count ++;
+	} while(checkingNextChar != EOF);
 
-    while (1) {
-        printf("\n\n you may now remove or add items to your list \n");
-        int result = CreateMenu(shoppingList);
-        printf("result is %d\n", result);
-        if (result == 1) {
-            fclose(filePtr);
-            break;
-        } else if (result == 2) {
-            printf("Enter product name\n");
-            scanf("%s", shoppingList[count]);
-            fprintf(filePtr, "%s\n", shoppingList[count]);
-            count++;
-        } else {
-            if (result <= count) {
-                //char tempShoppingList[20][50] = {};
-                for (int x = result - 1; x <= count - 1; x++) {
-                    strcpy(shoppingList[x], shoppingList[x + 1]); // assign arr[i+1] to arr[i]
-                }
-                strcpy(shoppingList[count - 1], "\0");
-                count--;
-                fclose(filePtr);
-                fclose(fopen(path, "w"));
-                filePtr = fopen(path, "a+");
-                for (int i = 2; i < count; ++i) {
+	while(1) {
+		printf("\n\n you may now remove or add items to your list \n");
+		int result = CreateMenu(shoppingList);
+		printf("result is %d\n", result);
+		if(result == 1) {
+			fclose(filePtr);
+			break;
+		} else if(result == 2) {
+			printf("Enter product name\n");
+			scanf("%s", shoppingList[count]);
+			fprintf(filePtr, "%s\n", shoppingList[count]);
+			count ++;
+		} else {
+			if(result <= count) {
+				//char tempShoppingList[MAX_OPTIONS][MAX_STRLEN] = {};
+				for(int x = result - 1; x <= count - 1; x ++) {
+					strcpy(shoppingList[x], shoppingList[x + 1]); // assign arr[i+1] to arr[i]
+				}
+				strcpy(shoppingList[count - 1], "\0");
+				count --;
+				fclose(filePtr);
+				fclose(fopen(path, "w"));
+				filePtr = fopen(path, "a+");
+				for(int i = 2; i < count; ++ i) {
 
-                    fprintf(filePtr, "%s", shoppingList[i]);
-                    if (i < count - 1) {
-                        fprintf(filePtr, "\n");
-                    }
-                }
-            }
-        }
-    }
+					fprintf(filePtr, "%s", shoppingList[i]);
+					if(i < count - 1) {
+						fprintf(filePtr, "\n");
+					}
+				}
+			}
+		}
+	}
 }
--- src/main.c
+++ src/main.c
@@ -5,7 +5,9 @@
 #include <stdio.h>
 #include "main.h"
 #include "CreateMenu.h"
+#include "database.h"
 #include "ListEditor.h"
+#include "search.h"
 /*
  * This function has the main loop and displays / returns the menu selected.
  * To make your own menu just copy this code and #include "CreateMenu.h"
@@ -14,7 +16,7 @@
  * #################################################################
  */
 void MainMenuScreen(){
-    char arr[20][50] = {"quit", "Individual Search", "List (load, make and search a list)", "Settings (store preferance etc)"};
+    char arr[MAX_OPTIONS][MAX_STRLEN] = {"quit", "Individual Search", "List (load, make and search a list)", "Settings (store preferance etc)"};
     int exitProgram = 0;
 
     while(exitProgram == 0) {
@@ -22,30 +24,33 @@ void MainMenuScreen(){
         switch (result) {
             case 1:
                 // Ending program by letting it return to main and end naturally instead of using (exit)
-                printf("Ending program...");
+                printf("Ending program...\n");
                 exitProgram = 1;
                 break;
             case 2:
                 // Open up the Search menu and start the search function
-                printf("Opening Search menu");
+                printf("Opening Search menu\n");
+		        data_t* data = LoadDatabase();
+		        ProductSearch(data);
                 break;
             case 3:
                 // Open up the List menu and start the List function
-                printf("Opening List menu");
+                printf("Opening List menu\n");
                 ListEditor();
                 break;
             case 4:
                 // Open up the Settings menu and start the Settings function
-                printf("Opening Settings menu");
+                printf("Opening Settings menu\n");
                 break;
             default:
-                // no valid input got sendt..... retrying.
-                printf("%d is not a valid input", result);
+                // no valid input got sent..... retrying.
+                printf("%d is not a valid input\n", result);
         }
     }
 }
 
-//int main(){
-//    MainMenuScreen();
+int main(){
 
-//}
+    MainMenuScreen();
+
+}
