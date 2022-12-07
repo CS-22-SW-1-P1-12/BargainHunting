463b759bd766367a84bca1262f042ea305acd7b7
ihem22 <ihem22@student.aau.dk>
2022-12-07 12:18:47 +0100

    ListSearch added, not functional

--- src/ListSearch.c
+++ src/ListSearch.c
@@ -4,8 +4,19 @@
 // Husk at definere ListSearch i ListEditor.
 #include "ListSearch.h"
 #include "database.h"
+#include "search.h"
+#include <stdlib.h>
 
-void ListSearch(data_t* data, char **shoppingList)
-{
+void ListSearch(data_t* data, char **shoppingList) {
+    int indexOfFoundProducts[MAX_FOUND_PRODUCTS];
+    for (int i = 0; shoppingList[i] != NULL; i++){
+        int numberOfFoundProducts = SearchData(shoppingList[i], data, indexOfFoundProducts);
+
+        char stores[MAX_STR_LEN];
+        for(int x = 0; numberOfFoundProducts > x; x++){
+
+        }
+    }
+
 
 }
