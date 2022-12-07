cae4ea4ebb537c79b89d7756628a3a7eb1720b56
ihem22 <ihem22@student.aau.dk>
2022-12-07 09:17:48 +0100

    ListSearch added, not functional

---
+++ src/ListSearch.c
@@ -0,0 +1,11 @@
+//
+// Created by Bruger on 05-12-2022.
+//
+// Husk at definere ListSearch i ListEditor.
+#include "ListSearch.h"
+#include "database.h"
+
+void ListSearch(data_t* data, char **shoppingList)
+{
+
+}
