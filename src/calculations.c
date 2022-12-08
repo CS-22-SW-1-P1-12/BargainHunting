d54487c2ac3472365c26d3a7e677817dd5c9747b
xmilla99 <kbja22@aau.student.dk>
2022-12-08 14:44:00 +0100

    initial work on func

--- src/calculations.c
+++ src/calculations.c
@@ -1,3 +1,60 @@
 //
 // Created by sinag on 17.11.2022.
 //
+#include "stdio.h"
+#include "database.h"
+#include "search.h"
+#include "CreateMenu.h"
+#include "string.h"
+#include "ListSearch.h"
+
+void Calculations (product_t **storeProducts){
+
+
+    //use Sinas functions to get lengths
+    char results[10][3]; //10 skal ændres
+
+
+    for (int i = 0; storeProducts[i] != NULL  ; ++i) {
+
+        double totalPrice = 0, totalPricePerKilo = 0;
+        char *storeName = storeProducts[i]->name;
+
+        for (int j = 0; j < 5; ++j) { //fix the length thing!!
+
+            if (storeProducts[j]->store == storeProducts[j-1]->store || j == 0) { //checks if store name is the same and increments total prices for this store if true
+                totalPrice += storeProducts[i]->price;
+                totalPricePerKilo += storeProducts[i]->pricePerKilo;
+            }
+
+        }
+
+        results[i][0] = *storeName;
+        results[i][1] = totalPrice;
+        results[i][2] = totalPricePerKilo;
+
+
+    }
+
+
+    int x = 0, counter = 0;
+    char temp[1][1];
+
+
+    while(x < 10){ //fix the length skal være nr of stores og ikke 10
+
+        if(results[x][1] > results[x+1][1]){
+
+            temp[0] = results[x];
+            results[x] = results[x+1];
+            results[x+1] = temp[0];
+        }
+
+        ++x;
+    }
+
+
+
+
+}
+
