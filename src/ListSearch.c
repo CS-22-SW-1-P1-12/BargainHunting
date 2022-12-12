7da218f23447d9c9180f9b258b5ff04df7160c92
sinagaming69 <sgrun22@student.aau.dk>
2022-12-12 10:29:42 +0100

    removed cry

--- src/ListSearch.c
+++ src/ListSearch.c
@@ -29,12 +29,6 @@ product_t** InitListSearch(FILE* filePtr){
         storeProducts[i] = malloc(sizeof(product_t) * data->productSize);
     }
     ListSearch(data, shoppingList, stores, storeProducts, numberOfStores, numberOfProducts);
-    for (int i = 0; i < numberOfStores; i++) {
-        printf("Store: %s\n", stores[i]);
-        for (int x = 0; x < numberOfProducts[i]; x++) {
-            printf("%s \n", storeProducts[i][x].name);
-        }
-    }
     return storeProducts;
 }
 
@@ -44,15 +38,11 @@ void ListSearch(data_t* data, char **shoppingList, char** stores, product_t** st
     }
     int indexOfFoundProducts[MAX_FOUND_PRODUCTS];
     for (int i = 0; shoppingList[i] != NULL; i++){
-        printf("searching for %s.\n", shoppingList[i]);
         int numberOfFoundProducts = SearchData(shoppingList[i], data, indexOfFoundProducts);
-        printf("cry\n");
         for(int x = 0; x < numberOfStores; x++){
             int* storeProductsTemp = malloc(sizeof(int) * numberOfFoundProducts);
             int numberOfTempProducts = 0;
-            printf("cry\n");
             for(int y = 0; y < numberOfFoundProducts; y++) {
-                printf("cry\n");
                 if (strcmp(data->products[indexOfFoundProducts[y]].store, stores[x]) == 0){
                     storeProductsTemp[numberOfTempProducts] = indexOfFoundProducts[y];
                     numberOfTempProducts++;
